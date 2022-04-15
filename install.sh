c31b66da92d1c110587701e79b4a5bba10f5a723
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-04-15 03:59:38 -0300

    feat: add docker, docker-copose to install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,22 @@ echo "1.9 Telegram"
 snap install telegram-desktop
 echo "1.10 neofetch"
 sudo apt install neofetch
+echo "1.11 Docker Engine"
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+echo "1.12 docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 echo "x. Post install"
 echo "x.1 Configuring fnm"
 fnm install 14
