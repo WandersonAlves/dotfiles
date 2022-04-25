af473eecb449f70b3d4fbf63254c740fabe2ebb0
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-04-25 03:10:57 -0300

    feat: remove snap installs

--- install.sh
+++ install.sh
@@ -1,31 +1,38 @@
 #!/bin/sh
 
+echo "⚡️ apt installs"
+sudo apt install git curl neofetch terminator wget gpg apt-transport-https
 echo "⚡️ 0. Install zsh and oh-my-zsh"
 sudo apt install zsh
-chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "⚡️ 1. Installing common applications"
 echo "⚡️ 1.1 fnm"
 curl -fsSL https://fnm.vercel.app/install | bash
 echo "⚡️ 1.2 vscode"
-snap install --classic code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt update
+sudo apt install code
 echo "⚡️ 1.3 Spotify"
-snap install spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install spotify-client
 echo "⚡️ 1.4 Google Chrome Stable"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
 sudo dpkg -i chrome.deb
-echo "⚡️ 1.5 Terminator (terminal emulator)"
-sudo apt install terminator
 echo "⚡️ 1.6 Calibe (ebook managment)"
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 echo "⚡️ 1.7 Slack"
-snap install slack
+wget https://downloads.slack-edge.com/releases/linux/4.25.0/prod/x64/slack-desktop-4.25.0-amd64.deb -O slack.deb
+sudo dpkg -i slack.deb
 echo "⚡️ 1.8 Postman"
-snap install postman
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+tar -xf postman.tar.gz -C $HOME/dev
 echo "⚡️ 1.9 Telegram"
-snap install telegram-desktop
-echo "⚡️ 1.10 neofetch"
-sudo apt install neofetch
+sudo apt install telegram-desktop
 echo "⚡️ 1.11 Docker Engine"
 sudo apt-get update
 sudo apt-get install \
