dff4ef264395385eda16d8b070c39c7c3676af38
Wanderson Ferreira <wferreira@questrade.com>
2022-04-26 15:35:47 -0300

    fix: fnm script

--- pre-installs/install-fnm.sh
+++ pre-installs/install-fnm.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-echo "⚡️ Installing fnm"
-curl -fsSL https://fnm.vercel.app/install | bash
-source ~/.zshrc
+if [ $(which fnm) != "$HOME/.fnm/fnm" ]; then
+    echo "⚡️ Installing fnm"
+    curl -fsSL https://fnm.vercel.app/install | bash
+    source ~/.zshrc
 
-echo "⚡️ Configuring node"
-fnm install 14
-fnm install 16
-fnm default 16
-
-echo "⚡️ Installing node global packages"
-npm config set ignore-scripts false
-npm i -g spaceship-prompt
-npm i -g npm
-npm i -g yarn
-npm i -g yeelight-manager
+    echo "⚡️ Configuring node"
+    fnm install 14
+    fnm install 16
+    fnm default 16
+    echo "Please refresh this terminal session or open another and run this script again"
+else
+    echo "⚡️ Installing node global packages"
+    npm config set ignore-scripts false
+    npm i -g spaceship-prompt
+    npm i -g npm
+    npm i -g yarn
+    npm i -g yeelight-manager
+fi
