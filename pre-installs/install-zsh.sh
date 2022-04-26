987a530e1660dd4d3ffcefb64408e1a05785b7d5
Wanderson Ferreira <wferreira@questrade.com>
2022-04-26 15:27:49 -0300

    feat: install zsh-autosuggestions/syntax-highlighting

--- pre-installs/install-zsh.sh
+++ pre-installs/install-zsh.sh
@@ -3,6 +3,8 @@
 if [ $SHELL = "/usr/bin/zsh" ]; then
     echo "⚡️ zsh installed, installing oh-my-zsh"
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     cp ../.zshrc ~/.zshrc
 else
     echo "⚡️ installing zsh"
