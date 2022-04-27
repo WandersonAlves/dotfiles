e445a815d3f9a1a4ca9e2ff566e390e0ee3582d5
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-04-26 21:44:28 -0300

    fix: konsole installation

--- themes.sh
+++ themes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install konsole
+sudo apt install konsole -y
 mkdir $HOME/.local/share/konsole
-cp -a ./files/konsole/profile $HOME/.local/share/konsole/
+cp -r ./files/konsole/profile $HOME/.local/share/konsole
 cp ./files/konsole/konsolerc $HOME/.config/konsolerc
