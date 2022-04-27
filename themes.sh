00724096e2c071c961d8e92bd61f543a735c1500
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-04-26 21:54:32 -0300

    fix: konsole

--- themes.sh
+++ themes.sh
@@ -2,5 +2,5 @@
 
 sudo apt install konsole -y
 mkdir $HOME/.local/share/konsole
-cp -r ./files/konsole/profile $HOME/.local/share/konsole
+cp -r ./files/konsole/profile/ $HOME/.local/share/konsole
 cp ./files/konsole/konsolerc $HOME/.config/konsolerc
