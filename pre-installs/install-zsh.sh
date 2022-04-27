2423a23689aa7e57d1ff5af64a8ecbd10cebcc28
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-04-26 22:13:34 -0300

    fix: zsh themes

--- pre-installs/install-zsh.sh
+++ pre-installs/install-zsh.sh
@@ -7,13 +7,13 @@ if [ $SHELL = "/usr/bin/zsh" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-    cp ../files/zsh/.zshrc ~/.zshrc
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt" --depth=1
+    ln -s "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 else
     echo "⚡️ installing zsh"
     sudo apt install zsh -y
     chsh -s $(which zsh)
+    cp ../files/zsh/.zshrc ~/.zshrc
     echo "Please logout and run this script again"
 
 fi
