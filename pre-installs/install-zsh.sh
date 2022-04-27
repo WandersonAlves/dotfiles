bfb42dbac0f3faa32ee14a386929feb8d728ebb0
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-04-26 22:40:57 -0300

    fix: git clone path

--- pre-installs/install-zsh.sh
+++ pre-installs/install-zsh.sh
@@ -7,7 +7,7 @@ if [ $SHELL = "/usr/bin/zsh" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/custom/themes/spaceship-prompt --depth=1
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
     ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/spaceship.zsh-theme
     cp ./files/zsh/.zshrc ~/.zshrc
 else
