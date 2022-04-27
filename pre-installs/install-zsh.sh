9a9159f5030d10dcf48f089835bad7ffd141ea90
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-04-26 22:26:16 -0300

    fix: create ZSH_CUSTOM on session

--- pre-installs/install-zsh.sh
+++ pre-installs/install-zsh.sh
@@ -5,10 +5,11 @@ if [ $SHELL = "/usr/bin/zsh" ]; then
     echo "⚠️ NOTE: oh-my-zsh will appear after been installed. Run `exit` to continue the script"
     sleep 2
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    ZSH_CUSTOM = $ZSH/custom
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt" --depth=1
-    ln -s "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 else
     echo "⚡️ installing zsh"
     sudo apt install zsh -y
