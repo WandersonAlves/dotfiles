c4e8bd4fceb611999b25c70c3b25b8f94368165d
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-04-26 21:41:30 -0300

    feat: add konsolerc

--- themes.sh
+++ themes.sh
@@ -2,4 +2,5 @@
 
 sudo apt install konsole
 mkdir $HOME/.local/share/konsole
-cp -a ./files/konsole $HOME/.local/share/konsole
+cp -a ./files/konsole/profile $HOME/.local/share/konsole/
+cp ./files/konsole/konsolerc $HOME/.config/konsolerc
