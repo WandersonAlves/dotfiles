15b8376636c5b6523784c29e6950292514866cbd
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-05-01 03:35:45 -0300

    feat: use python script to set custom keyboard shortcuts

--- themes.sh
+++ themes.sh
@@ -6,7 +6,9 @@ cp -r ./files/konsole/profile/. $HOME/.local/share/konsole
 cp ./files/konsole/konsolerc $HOME/.config/konsolerc
 
 echo "⚡️ 1.1 ULauncher"
-sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
+sudo apt install pip -y
+sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher -y
 git clone https://github.com/KiranWells/ulauncher-nord/ \
     ~/.config/ulauncher/user-themes/nord
 cp -r ./files/ulauncher/. $HOME/.config/ulauncher
+python3 ./set-keyboard-shortcut.py 'ULauncher' 'ulauncher-toggle' '<Super>Space'
