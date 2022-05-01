2d7e7305896f37f3b35b87308d4adf6e8dc7d67e
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2022-05-01 04:11:39 -0300

    feat: add shortcuts via terminal

--- themes.sh
+++ themes.sh
@@ -4,6 +4,8 @@ sudo apt install konsole -y
 mkdir $HOME/.local/share/konsole
 cp -r ./files/konsole/profile/. $HOME/.local/share/konsole
 cp ./files/konsole/konsolerc $HOME/.config/konsolerc
+gsettings set org.gnome.settings-daemon.plugins.media-keys terminal '[]'
+python3 ./set-keyboard-shortcut.py 'Konsole' 'konsole' '<Control><Alt>T'
 
 echo "⚡️ 1.1 ULauncher"
 sudo apt install pip -y
@@ -11,4 +13,6 @@ sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt 
 git clone https://github.com/KiranWells/ulauncher-nord/ \
     ~/.config/ulauncher/user-themes/nord
 cp -r ./files/ulauncher/. $HOME/.config/ulauncher
-python3 ./set-keyboard-shortcut.py 'ULauncher' 'ulauncher-toggle' '<Super>Space'
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source '[]'
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward '[]'
+python3 ./set-keyboard-shortcut.py 'ULauncher' 'ulauncher-toggle' '<Super>space'
