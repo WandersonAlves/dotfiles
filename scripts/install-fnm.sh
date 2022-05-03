65f48409f2abf1a65fa8ab99fcb9d09ce5d186a7
Wanderson Ferreira <wferreira@questrade.com>
2022-05-03 11:34:56 -0300

    fix: fnm

--- scripts/install-fnm.sh
+++ scripts/install-fnm.sh
@@ -3,18 +3,17 @@
 WHICH_FNM=$(which fnm)
 
 if [ "${WHICH_FNM}" = "$HOME/.fnm/fnm" ]; then
+    echo "⚡️ Configuring node"
+    fnm install 14
+    fnm install 16
+    fnm default 16
     echo "⚡️ Installing node global packages"
     npm i -g npm yarn yeelight-manager
 else
     if [ $SHELL = "/usr/bin/zsh" ]; then
         echo "⚡️ Installing fnm"
         curl -fsSL https://fnm.vercel.app/install | bash
-        source ~/.zshrc
 
-        echo "⚡️ Configuring node"
-        fnm install 14
-        fnm install 16
-        fnm default 16
         echo "Please refresh this terminal session or open another and run this script again"
     else
         echo "ERROR: You need to have zsh installed to install fnm"
