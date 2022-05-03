2ca97bc477000ed0b932f10623770936cabc60db
Wanderson Ferreira <wferreira@questrade.com>
2022-05-03 11:56:10 -0300

    fix: remove installation files

--- scripts/applications/postman.sh
+++ scripts/applications/postman.sh
@@ -2,3 +2,4 @@
 
 wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
 tar -xf postman.tar.gz -C $HOME/dev
+rm postman.tar.gz
--- scripts/applications/slack.sh
+++ scripts/applications/slack.sh
@@ -2,3 +2,4 @@
 
 wget https://downloads.slack-edge.com/releases/linux/4.25.0/prod/x64/slack-desktop-4.25.0-amd64.deb -O slack.deb
 sudo dpkg -i slack.deb
+rm slack.deb
